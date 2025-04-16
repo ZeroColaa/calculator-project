aebe1a94e299a51f74964f9cad69bc6d0b9abd84
ksy <ksy@ksyui-MacBookPro.local>
2025-04-16 19:26:30 +0900

    Lv2

---
+++ src/lv2/App.java
@@ -0,0 +1,102 @@
+package lv2;
+
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        Scanner sc = new Scanner(System.in);
+        boolean flag = true;
+
+        while (flag) {
+            int firstNumber = 0;
+            int secondNumber = 0;
+
+            //첫번째 피연산자
+            while (true) {
+                try {
+                    System.out.print("첫번째 연산자(양의 정수)를 입력하세요: ");
+                    firstNumber = Integer.parseInt(sc.nextLine());
+                    if (firstNumber < 0) {
+                        System.out.println("양의 정수만 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("양의 정수만 입력해주세요.");
+                }
+            }
+
+            //두번째 피연산자
+            while (true) {
+                try {
+                    System.out.print("두번째 연산자(양의 정수)를 입력하세요: ");
+                    secondNumber = Integer.parseInt(sc.nextLine());
+                    if (secondNumber < 0) {
+                        System.out.println("양의 정수만 입력해주세요.");
+                        continue;
+                    }
+                    break;
+                } catch (NumberFormatException e) {
+                    System.out.println("양의 정수만 입력해주세요.");
+                }
+            }
+
+            char operator;
+            //연산자
+            while (true) {
+                System.out.print("연산자를 입력해주세요 (+, -, x, /): ");
+                String opInput = sc.nextLine();
+                if (opInput.isEmpty()) {
+                    continue;
+                }
+                operator = opInput.charAt(0);
+                if ("+-x/".indexOf(operator) == -1) {
+                    System.out.println("연산자를 정확히 입력해주세요.");
+                    continue;
+                }
+                break;
+            }
+
+
+            //계산
+            int result = calculator.calculate(firstNumber, secondNumber, operator);
+            System.out.println("결과: " + result);
+
+            System.out.println("저장된 연산 결과들: " + calculator.getResult());
+
+            System.out.println("가장 오래된 결과를 삭제하시겠습니까? (y/n): ");
+            String answer = sc.nextLine();
+            if (answer.equals("y")) {
+                calculator.removeResult();
+                System.out.println("삭제 완료.");
+                System.out.println("현재 결과 목록: " + calculator.getResult());
+            }
+
+            System.out.println("요소를 수정하시겠습니까? (y/n): ");
+            String answer2 = sc.nextLine();
+            if (answer2.equals("y")) {
+                System.out.print("수정할 인덱스를 입력하세요: ");
+                int index = Integer.parseInt(sc.nextLine());
+
+                System.out.print("새로운 값을 입력하세요: ");
+                int newValue = Integer.parseInt(sc.nextLine());
+
+                calculator.setResult(index, newValue);
+                System.out.println("수정 후 결과 목록: " + calculator.getResult());
+            }
+
+            //반복 여부 묻기
+            System.out.print("더 계산하시겠습니까? (exit 입력 시 종료): ");
+            String isExit = sc.nextLine();
+            if (isExit.equals("exit")) {
+                flag = false;
+                System.out.println("종료합니다.");
+            }
+
+        }
+        sc.close();
+
+    }
+
+}
---
+++ src/lv2/Calculator.java
@@ -0,0 +1,59 @@
+package lv2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Calculator {
+
+    //연산 결과 저장(캡슐화)
+    private List<Integer> result;
+
+    //생성자
+    public Calculator(){
+        this.result = new ArrayList<>();
+    }
+    //사칙연산 수행, 결과 반환 및 저장
+    public int calculate(int a, int b, char operator){
+        int calcResult=0;
+
+        switch (operator){
+            case '+':
+                calcResult = a + b;
+                break;
+            case '-':
+                calcResult = a - b;
+                break;
+            case 'x':
+                calcResult = a * b;
+                break;
+            case '/':
+                if (b==0) throw new ArithmeticException("0으로 나눌 수 없습니다.");
+                calcResult=a/b;
+                break;
+            default:
+                throw new IllegalArgumentException("지원하지 않는 연산자입니다.");
+        }
+        result.add(calcResult);//결과 저장
+        return calcResult; //결과 반환
+    }
+
+    //getter
+    public List<Integer> getResult(){
+        return result;
+    }
+
+    //setter
+    public void setResult(int index, int newValue){
+        if(index>=0&&index<result.size()){
+            result.set(index,newValue);
+        }else{
+            System.out.println("잘못된 인덱스입니다.");
+        }
+    }
+    //가장 먼저 저장된 결과 제거
+    public void removeResult(){
+        if(!result.isEmpty()){
+            result.remove(0);
+        }
+    }
+}
